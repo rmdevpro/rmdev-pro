bceafc34a909381a71ccdb59fe6a1a42d844ad0d
rmdevpro <j@rmdev.pro>
2025-10-20 08:43:40 +0000

    Deploying to gh-pages from @ rmdevpro/rmdev-pro@11b9e293d75c0efe7149bb4a76ab0a42af1458a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Joshua ecosystem projects and initiatives.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/rmdev-pro/projects/";
